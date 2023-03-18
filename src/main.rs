70122ca9d9b2296024a8a0627d06ebdd91215c76
Nightingale <r.nightingale@uq.edu.au>
2023-03-18 14:56:38 +1000

    Changed over to using the crossterm module for terminal UI.

--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,85 @@
-extern crate ncurses;
+//extern crate ncurses;
 
-use std::collections::HashMap;
+use std::io::{
+    stdout, Write
+};
 use rand::Rng;
 
-struct TermColorPair {
-    id : i16,
-}
-
-struct TermColor {
-    nextid : i16,
-    pairs : HashMap<String, TermColorPair>,
-}
-
-/* Wrapper around ncurses color pairs */
-impl TermColorPair {
-    fn new(id: i16, bg: i16, fg: i16) -> TermColorPair {
-        ncurses::init_pair(id, bg, fg);
-        TermColorPair{id}
-    }
-    fn addstr(&self, x: i32, y: i32, s: &str) {
-        ncurses::attron(ncurses::COLOR_PAIR(self.id.try_into().unwrap()));
-        ncurses::mvaddstr(y, x, s);
-        ncurses::attroff(ncurses::COLOR_PAIR(self.id.try_into().unwrap()));
-    }
-    fn addc(&self, x: i32, y: i32, c: char) {
-        ncurses::attron(ncurses::COLOR_PAIR(self.id.try_into().unwrap()));
-        ncurses::mvaddch(y, x, c as u32);
-        ncurses::attroff(ncurses::COLOR_PAIR(self.id.try_into().unwrap()));
-    }
-}
-
-impl TermColor {
-    fn new() -> TermColor {
-        TermColor{nextid : 1, pairs : HashMap::new()}
-    }
-    fn add_pair(&mut self, name: &str, bg: i16, fg: i16) {
-        let tcp = TermColorPair::new(self.nextid, bg, fg);
-        self.pairs.insert(String::from(name), tcp);
-        self.nextid += 1;
-    }
-    fn get(&self, name: &str) -> &TermColorPair {
-        self.pairs.get(name).unwrap()
-    }
-}
+use crossterm::{
+    queue, event,
+    style::{Color, Print, ResetColor, SetForegroundColor},
+    terminal, cursor,
+    ExecutableCommand, Result,
+};
 
 struct Room {
-    xsize: i32,
-    ysize: i32,
-    dust : Vec<(i32, i32)>,
+    xsize: u16,
+    ysize: u16,
+    dust : Vec<(u16, u16)>,
 }
 
 impl Room {
-    fn new(xsize: i32, ysize: i32) -> Room {
+    fn new(xsize: u16, ysize: u16) -> Room {
         let mut rng = rand::thread_rng();
-        let mut dust: Vec<(i32, i32)> = Vec::new();
-        for i in 0..((xsize * ysize) / 10) {
+        let mut dust: Vec<(u16, u16)> = Vec::new();
+        for _ in 0..((xsize * ysize) / 10) {
             let x = rng.gen_range(0..xsize);
             let y = rng.gen_range(0..ysize);
             dust.push((x, y));
         }
         Room{xsize, ysize, dust}
     }
+    fn draw(&self) -> Result<()> {
+        let mut stdout = stdout();
+        queue!(stdout, terminal::Clear(terminal::ClearType::All))?;
+
+        for (x, y) in &self.dust {
+            queue!(stdout,
+                   cursor::MoveTo((2 * x) + 1, y + 1),
+                   SetForegroundColor(Color::Red),
+                   Print("<>"),
+                   //Print("\u{26AA}"),
+                   ResetColor)?;
+        }
+
+        /* Draw border around room */
+        queue!(stdout, cursor::MoveTo(0, 0), Print("\u{2554}"))?;
+        for _ in 0..self.xsize {
+            queue!(stdout, Print("\u{2550}\u{2550}"))?;
+        }
+        queue!(stdout, Print("\u{2557}"))?;
+        queue!(stdout, cursor::MoveTo(0, self.ysize + 1), Print("\u{255A}"))?;
+        for _ in 0..self.xsize {
+            queue!(stdout, Print("\u{2550}\u{2550}"))?;
+        }
+        queue!(stdout, Print("\u{255D}"))?;
+        for i in 0..self.ysize {
+            queue!(stdout,
+                   cursor::MoveTo(0, i + 1),
+                   Print("\u{2551}"),
+                   cursor::MoveTo(self.xsize * 2 + 1, i + 1),
+                   Print("\u{2551}"))?;
+        }
+        queue!(stdout,
+               cursor::MoveTo(2, 0),
+               SetForegroundColor(Color::Green),
+               Print("Robot Vacuum Simulator!"),
+               ResetColor)?;
+
+        stdout.flush()?;
+        Ok(())
+    }
 }
 
 fn main() {
-    let mut win = ncurses::initscr();
-    ncurses::start_color();
-
-    let (w, h) = (ncurses::getmaxx(win), ncurses::getmaxy(win));
+    let (w, h) = terminal::size().unwrap(); 
+    let (w, h) = ((w - 2) / 2, h - 8);
     
-    /* Colors */
-    let mut tc = TermColor::new();
-    tc.add_pair("green", ncurses::COLOR_GREEN, ncurses::COLOR_BLACK);
-    tc.add_pair("red", ncurses::COLOR_RED, ncurses::COLOR_BLACK);
-    
-    /* Game window */
-    let game_win = ncurses::newwin(h - 6, w, 0, 0);
-    ncurses::wborder(game_win, 0, 0, 0, 0, 0, 0, 0, 0);
-    ncurses::refresh();
-    ncurses::wrefresh(game_win);
+    let room = Room::new(w, h);
+    _ = room.draw();
 
-    tc.get("green").addstr(1, 0, "Robot Vacuum Simulator!");
-
-    let room = Room::new(w - 2, h - 8);
-
-    for (x, y) in room.dust {
-        tc.get("red").addc(x + 1, y + 1, 'x');
-    }
-    
-    while true {
-        let c = ncurses::getch();
+    loop {
+        //let c = ncurses::getch();
     }
     
 }
